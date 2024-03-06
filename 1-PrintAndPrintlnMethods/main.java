d06357c4c26b2867bf4cb043a6361f36dbef8077
Dogukan Koc <dk.dogukankoc@gmail.com>
2024-03-06 09:43:35 +0300

    Created first source code for java. I tried print and println methods

---
+++ 1-PrintAndPrintlnMethods/main.java
@@ -0,0 +1,16 @@
+public class Main 
+{ 
+
+
+   public static void main(String[] blabla)
+   {
+
+      System.out.println("Hi");
+      System.out.println("How are you?");
+    
+      System.out.print("I'm fine ");
+      System.out.print("you?");
+   }
+
+
+}
