5ba98cff1b72c23506dfc2273646138fde65e620
Dogukan Koc <dk.dogukankoc@gmail.com>
2024-03-07 09:26:21 +0300

    Some variable exercises

---
+++ 2-Variables/main.java
@@ -0,0 +1,52 @@
+public class Main 
+{
+	public static void main(String[] args)
+    {
+    	String name = "Dogukan";
+        System.out.println(name);
+        
+        int myNumber = 5;
+        System.out.println(myNumber);
+        
+        int myNum;
+        myNum = 25;
+        
+        System.out.println(myNum);
+        
+        
+        int myNumber1 = 111;
+        myNumber1 = 222;
+        System.out.println(myNumber1);
+        
+        float floatNumber = 5.334f;
+        System.out.println(floatNumber);
+        
+        char letter = 'D';
+        System.out.println(letter);
+        
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+        
+        String fullName = "Dogukan Koc";
+        System.out.println("Hi my name is " + fullName);
+        
+        String firstName = "Dogukan ";
+        String lastName = "Koc";
+        
+        String myName = firstName + lastName;
+        System.out.println(myName);
+        
+        int x = 5;
+        
+        int y = 7;
+        
+        float z = 5.56f;
+        
+        System.out.println(x + y);
+        
+        System.out.println(x + y + z);
+        
+        int a = 15, b = 25, c = 55;
+	System.out.println("a = " + a + ", b = " + b + ", c = " + c);
+    }
+}
